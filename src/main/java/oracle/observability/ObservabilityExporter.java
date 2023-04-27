47097b694a331cfd9fe8186a5d4d67368db37f02
Paul Parkinson <paul.parkinson@gmail.com>
2023-04-27 19:14:33 -0400

    DATA_SOURCE_USER/PW/SERVICENAME support and multi-database support (#18)
    * add list of default metrics in readme
    
    * readme updates
    
    * various updates
    
    * README.md updates
    
    * README.md updates
    
    * basic example
    
    * adds... 1. ability to specify DATA_SOURCE_USER, DATA_SOURCE_PASSWORD, and DATA_SOURCE_SERVICENAME individually rather than as one DATA_SOURCE_NAME entry (thus more secure) 2. multi-database support via "/scrape" endpoint with "target" param taking dns for specific database (this will soon be enhanced so that it is instead configurable, again to be more secure) . Also, updated POM entries for 21.7 version.

--- src/main/java/oracle/observability/ObservabilityExporter.java
+++ src/main/java/oracle/observability/ObservabilityExporter.java
@@ -6,21 +6,32 @@ import com.oracle.bmc.secrets.SecretsClient;
 import com.oracle.bmc.secrets.model.Base64SecretBundleContentDetails;
 import com.oracle.bmc.secrets.requests.GetSecretBundleRequest;
 import com.oracle.bmc.secrets.responses.GetSecretBundleResponse;
+import oracle.observability.metrics.MetricsExporter;
 import oracle.ucp.jdbc.PoolDataSource;
 import oracle.ucp.jdbc.PoolDataSourceFactory;
 import org.apache.commons.codec.binary.Base64;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ObservabilityExporter {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ObservabilityExporter.class);
     public String DEFAULT_METRICS = System.getenv("DEFAULT_METRICS"); // "default-metrics.toml"
+    public File DEFAULT_METRICS_FILE;
     public String CUSTOM_METRICS      = System.getenv("CUSTOM_METRICS"); //
     public String QUERY_TIMEOUT       = System.getenv("QUERY_TIMEOUT"); // "5"
     public String DATABASE_MAXIDLECONNS       = System.getenv("DATABASE_MAXIDLECONNS"); // "0"
     public String DATABASE_MAXOPENCONNS       = System.getenv("DATABASE_MAXOPENCONNS"); // "10"
-    public String DATA_SOURCE_NAME = System.getenv("DATA_SOURCE_NAME"); //eg %USER%/$(dbpassword)@%PDB_NAME%_tp
+    public static String DATA_SOURCE_NAME = System.getenv("DATA_SOURCE_NAME"); //eg %USER%/$(dbpassword)@%PDB_NAME%_tp
+    public static String DATA_SOURCE_USER = System.getenv("DATA_SOURCE_USER"); //eg %USER%/$(dbpassword)@%PDB_NAME%_tp
+    public static String DATA_SOURCE_PASSWORD = System.getenv("DATA_SOURCE_PASSWORD"); //eg %USER%/$(dbpassword)@%PDB_NAME%_tp
+    public static String DATA_SOURCE_SERVICENAME = System.getenv("DATA_SOURCE_SERVICENAME"); //eg %USER%/$(dbpassword)@%PDB_NAME%_tp
     public String TNS_ADMIN = System.getenv("TNS_ADMIN");  //eg /msdataworkshop/creds
     public String OCI_REGION = System.getenv("OCI_REGION");  //eg us-ashburn-1
     public String VAULT_SECRET_OCID = System.getenv("VAULT_SECRET_OCID");  //eg ocid....
@@ -29,27 +40,52 @@ public class ObservabilityExporter {
     public static final String CONTEXT = "context";
     public static final String REQUEST = "request";
 
+    static {
+
+        if (DATA_SOURCE_USER != null && DATA_SOURCE_PASSWORD != null && DATA_SOURCE_SERVICENAME != null) {
+            DATA_SOURCE_NAME = DATA_SOURCE_USER + "/" + DATA_SOURCE_PASSWORD + "@" + DATA_SOURCE_SERVICENAME;
+            LOGGER.info("DATA_SOURCE_NAME = DATA_SOURCE_USER + \"/\" + DATA_SOURCE_PASSWORD + \"@\" + DATA_SOURCE_SERVICENAME");
+            //eg %USER%/$(dbpassword)@%PDB_NAME%_tp
+        }
+    }
     PoolDataSource observabilityDB;
+    Map<String, PoolDataSource> dataSourceNameToDataSourceMap = new HashMap<>();
+
     public PoolDataSource getPoolDataSource() throws SQLException {
-        if (observabilityDB != null) return observabilityDB;
-        observabilityDB = PoolDataSourceFactory.getPoolDataSource();
-        observabilityDB.setConnectionFactoryClassName("oracle.jdbc.pool.OracleDataSource");
-        String user = DATA_SOURCE_NAME.substring(0, DATA_SOURCE_NAME.indexOf("/"));
-        String pw = DATA_SOURCE_NAME.substring(DATA_SOURCE_NAME.indexOf("/") + 1, DATA_SOURCE_NAME.indexOf("@"));
-        String serviceName = DATA_SOURCE_NAME.substring(DATA_SOURCE_NAME.indexOf("@") + 1);
+        return getPoolDataSource(DATA_SOURCE_NAME);
+    }
+    public PoolDataSource getPoolDataSource(String dataSourceName) throws SQLException {
+        if (dataSourceName.equals(DATA_SOURCE_NAME)) {
+            if (observabilityDB != null) return observabilityDB;
+            return observabilityDB = getDataSource(DATA_SOURCE_NAME);
+        } else {
+            if(dataSourceNameToDataSourceMap.containsKey(dataSourceName) && dataSourceNameToDataSourceMap.get(dataSourceName) != null)
+                return dataSourceNameToDataSourceMap.get(dataSourceName);
+            PoolDataSource poolDataSource = getDataSource(dataSourceName);
+            dataSourceNameToDataSourceMap.put(dataSourceName, poolDataSource);
+            return poolDataSource;
+        }
+    }
+
+    private PoolDataSource getDataSource(String dataSourceName) throws SQLException {
+        PoolDataSource poolDataSource = PoolDataSourceFactory.getPoolDataSource();
+        poolDataSource.setConnectionFactoryClassName("oracle.jdbc.pool.OracleDataSource");
+        String user = dataSourceName.substring(0, dataSourceName.indexOf("/"));
+        String pw = dataSourceName.substring(dataSourceName.indexOf("/") + 1, dataSourceName.indexOf("@"));
+        String serviceName = dataSourceName.substring(dataSourceName.indexOf("@") + 1);
         String url = "jdbc:oracle:thin:@" + serviceName + "?TNS_ADMIN=" + TNS_ADMIN;
-        observabilityDB.setURL(url);
-        observabilityDB.setUser(user);
-        if (VAULT_SECRET_OCID == null || VAULT_SECRET_OCID.trim().equals("")) {
-            observabilityDB.setPassword(pw);
+        poolDataSource.setURL(url);
+        poolDataSource.setUser(user);
+        if (VAULT_SECRET_OCID == null || VAULT_SECRET_OCID.trim().equals("") || !dataSourceName.equals(DATA_SOURCE_NAME)) {
+            poolDataSource.setPassword(pw);
         } else {
             try {
-                observabilityDB.setPassword(getPasswordFromVault());
+                poolDataSource.setPassword(getPasswordFromVault());
             } catch (IOException e) {
                 throw new SQLException(e);
             }
         }
-        return observabilityDB;
+        return poolDataSource;
     }
 
 
