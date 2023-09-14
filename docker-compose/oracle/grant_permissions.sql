59d4922483f278655c2f129b355c37d4ff25cc14
Mark Nelson <mark.x.nelson@oracle.com>
2023-09-14 09:32:01 -0400

    add txeventq info (#28)
    Signed-off-by: Mark Nelson <mark.x.nelson@oracle.com>

--- docker-compose/oracle/grant_permissions.sql
+++ docker-compose/oracle/grant_permissions.sql
@@ -1,2 +1,10 @@
 alter session set container=freepdb1;
 grant select_catalog_role to pdbadmin;
+grant execute on dbms_aq to pdbadmin;
+grant execute on dbms_aqadm to pdbadmin;
+grant execute on dbms_aqin to pdbadmin;
+grant execute on dbms_aqjms_internal to pdbadmin;
+grant execute on dbms_teqk to pdbadmin;
+grant execute on DBMS_RESOURCE_MANAGER to pdbadmin;
+grant select on sys.aq$_queue_shards to pdbadmin;
+grant select on user_queue_partition_assignment_table to pdbadmin;
