a793f22425edc0f307ba4edce7cc90282811b905
Mark Nelson <mark.x.nelson@oracle.com>
2025-04-18 11:24:19 -0400

    review commetns
    Signed-off-by: Mark Nelson <mark.x.nelson@oracle.com>

--- azvault/azvault.go
+++ azvault/azvault.go
@@ -6,6 +6,7 @@ package azvault
 import (
 	"context"
 	"fmt"
+	"os"
 	"strings"
 
 	"github.com/prometheus/common/promslog"
@@ -24,10 +25,15 @@ func GetVaultSecret(vaultId string, secretName string) string {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		logger.Error("Failed to obtain an Azure Credential", "err", err)
+		os.Exit(1)
 	}
 
 	// establish a connection to the key vault client
 	client, err := azsecrets.NewClient(vaultURI, cred, nil)
+	if err != nil {
+		logger.Error("Failed to create Azure Secrets Client", "err", err)
+		os.Exit(1)
+	}
 
 	// get the secret - empty string version means "latest"
 	version := ""
@@ -35,6 +41,7 @@ func GetVaultSecret(vaultId string, secretName string) string {
 	resp, err := client.GetSecret(context.TODO(), secretName, version, nil)
 	if err != nil {
 		logger.Error("Failed to get secret from vault", "err", err)
+		os.Exit(1)
 	} else {
 		secret = *resp.Value
 	}
