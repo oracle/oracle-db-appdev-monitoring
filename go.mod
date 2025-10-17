d42aeeddacca387a6e3ae36350a447ae51612075
Ilmar Kerm <ilmar@ilmarkerm.eu>
2025-10-17 18:04:59 +0200

    397 - HashiCorp Vault support for database secrets (#398)
    * Signed-off-by: Ilmar Kerm <ilmar.kerm@gmail.com>
    First edition of HashiCorp Vault support

--- go.mod
+++ go.mod
@@ -23,22 +23,37 @@ require (
 	github.com/VictoriaMetrics/easyproto v0.1.4 // indirect
 	github.com/alecthomas/units v0.0.0-20211218093645-b94a6e3cc137 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/coreos/go-systemd/v22 v22.6.0 // indirect
+	github.com/go-jose/go-jose/v4 v4.1.1 // indirect
 	github.com/go-logfmt/logfmt v0.6.0 // indirect
 	github.com/godror/knownpb v0.3.0 // indirect
 	github.com/gofrs/flock v0.8.1 // indirect
 	github.com/golang-jwt/jwt/v5 v5.3.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/hashicorp/go-retryablehttp v0.7.8 // indirect
+	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
+	github.com/hashicorp/go-secure-stdlib/parseutil v0.2.0 // indirect
+	github.com/hashicorp/go-secure-stdlib/strutil v0.1.2 // indirect
+	github.com/hashicorp/go-sockaddr v1.0.7 // indirect
+	github.com/hashicorp/hcl v1.0.1-vault-7 // indirect
+	github.com/hashicorp/vault/api v1.22.0 // indirect
 	github.com/jpillora/backoff v1.0.0 // indirect
 	github.com/kylelemons/godebug v1.1.0 // indirect
 	github.com/mdlayher/socket v0.4.1 // indirect
 	github.com/mdlayher/vsock v1.2.1 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/mwitkow/go-conntrack v0.0.0-20190716064945-2f068394615f // indirect
 	github.com/pkg/browser v0.0.0-20240102092130-5ac0b6a4141c // indirect
 	github.com/prometheus/client_model v0.6.2 // indirect
 	github.com/prometheus/procfs v0.16.1 // indirect
+	github.com/ryanuber/go-glob v1.0.0 // indirect
 	github.com/sony/gobreaker v0.5.0 // indirect
 	github.com/xhit/go-str2duration/v2 v2.1.0 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20240726163527-a2c0da244d78 // indirect
@@ -50,5 +65,6 @@ require (
 	golang.org/x/sync v0.16.0 // indirect
 	golang.org/x/sys v0.35.0 // indirect
 	golang.org/x/text v0.28.0 // indirect
+	golang.org/x/time v0.12.0 // indirect
 	google.golang.org/protobuf v1.36.8 // indirect
 )
---
+++ hashivault/hashivault.go
@@ -0,0 +1,116 @@
+// Copyright (c) 2025, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package hashivault
+
+import (
+	"context"
+	"strings"
+	"errors"
+	"net"
+	"net/http"
+	"time"
+	"github.com/oracle/oci-go-sdk/v65/example/helpers"
+
+	"log/slog"
+	vault "github.com/hashicorp/vault/api"
+)
+
+var UnsupportedMountType = errors.New("Unsupported HashiCorp Vault mount type")
+var RequiredKeyMissing = errors.New("Required key missing from HashiCorp Vault secret")
+
+type HashicorpVaultClient struct {
+	client *vault.Client
+	logger *slog.Logger
+}
+
+// newUnixSocketVaultClient creates a custom HTTP client using a Unix socket
+func newUnixSocketVaultClient(socketPath string) (*vault.Client, error) {
+	httpClient := &http.Client{
+		Transport: &http.Transport{
+			DialContext: func(_ context.Context, _, _ string) (net.Conn, error) {
+				return net.Dial("unix", socketPath)
+			},
+		},
+		Timeout: 10 * time.Second,
+	}
+
+	// Configure the Vault client
+	config := &vault.Config{
+		Address:      "http://unix",
+		HttpClient:   httpClient,
+		Timeout:      10 * time.Second,
+		MinRetryWait: time.Millisecond * 1000,
+		MaxRetryWait: time.Millisecond * 1500,
+		MaxRetries:   2,
+	}
+
+	return vault.NewClient(config)
+}
+
+// createVaultClient connects to a vault client, using connection method specified with the parameters. Returns error if fails.
+func createVaultClient(logger *slog.Logger, socketPath string) (HashicorpVaultClient,error) {
+	var vaultClient HashicorpVaultClient
+	var err error
+
+	if socketPath != "" {
+		// Create Vault client that uses Unix Socket
+		vaultClient.client, err = newUnixSocketVaultClient(socketPath)
+	}
+	if err != nil {
+		logger.Error("Failed to connect to HashiCorp Vault", "err", err)
+	}
+	vaultClient.logger = logger
+	return vaultClient,err
+}
+
+// CreateVaultClient connects to a vault client, using connection method specified with the parameters. Fatal if fails.
+func CreateVaultClient(logger *slog.Logger, socketPath string) HashicorpVaultClient {
+	c,err := createVaultClient(logger, socketPath)
+	helpers.FatalIfError(err)
+	return c
+}
+
+// getVaultSecret fetches secret from vault using specified mount type. Returns error on failure.
+func (c HashicorpVaultClient) getVaultSecret(mountType string, mount string, path string, requiredKeys []string) (map[string]string,error) {
+	result := map[string]string{}
+	var err error
+	if mountType == "kvv2" || mountType == "kvv1" {
+		// Handle simple key-value secrets
+		var secret *vault.KVSecret
+		c.logger.Info("Making call to HashiCorp Vault", "mountType", mountType, "mountName", mount, "secretPath", path, "expectedKeys", requiredKeys)
+		if mountType == "kvv2" {
+			secret, err = c.client.KVv2(mount).Get(context.TODO(), path)
+		} else {
+			secret, err = c.client.KVv1(mount).Get(context.TODO(), path)
+		}
+		if err != nil {
+			c.logger.Error("Failed to fetch secret from HashiCorp Vault", "err", err)
+			return result, err
+		}
+		// Expect simple one-level JSON, remap interface{} straight to string
+		for key,val := range secret.Data {
+			result[key] = strings.TrimRight(val.(string), "\r\n") // make sure a \r and/or \n didn't make it into the secret
+		}
+	} else {
+		c.logger.Error(UnsupportedMountType.Error())
+		return result, UnsupportedMountType
+	}
+	// Check that we have all required keys present
+	for _, key := range requiredKeys {
+		val, keyExists := result[key]
+		if !keyExists || val == "" {
+			c.logger.Error(RequiredKeyMissing.Error(), "key", key)
+			return result, RequiredKeyMissing
+		}
+	}
+	return result, nil
+}
+
+// GetVaultSecret fetches secret from vault using specified mount type. Fatal on failure.
+func (c HashicorpVaultClient) GetVaultSecret(mountType string, mount string, path string, requiredKeys []string) map[string]string {
+	// Public callable function that does not return an error, just exits instead. Like other vault code in this project.
+	res,err := c.getVaultSecret(mountType, mount, path, requiredKeys)
+	helpers.FatalIfError(err)
+	return res
+}
