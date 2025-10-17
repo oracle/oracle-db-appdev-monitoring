083333934fe11a63db9a397e7a754494dcc17a72
Ilmar Kerm <ilmar@ilmarkerm.eu>
2025-10-17 11:57:57 +0200

    Moved into HashiCorp recommended Docker based testing

--- hashivault/hashivault_test.go
+++ hashivault/hashivault_test.go
@@ -2,26 +2,61 @@ package hashivault
 
 import (
 	"testing"
+	"log/slog"
+	vault "github.com/hashicorp/vault/api"
+	// requires go1.25
+	"github.com/hashicorp/vault/sdk/helper/testcluster/docker"
 )
 
 /*
-	Performs some integration tests against a running Vault proxy
+	Removing the tests until project is upgraded to go1.25
+	https://github.com/hashicorp/vault?tab=readme-ov-file#docker-based-tests
 */
 
 const (
-	// TODO: Mock the entire Vault response and do not depend on external Vault
-	socketPath = "/var/run/vault/vault.sock"
-	testMount = "dev.mt1"
-	testPath = "oracle/devdbs01/monitoring"
+	dockerImageRepo = "hashicorp/vault"
+	dockerImageTag = "latest"
+	kvTestMount = "secret"
+	kvTestPath = "foo1"
+	kvTestSecret = map[string]string{
+		"username": "c##monitoring",
+		"password": "ep82^RxU>iqE%ZMWr!}UmtM50?~C@P",
+		"user": "monitoring",
+		"pass": "kz)7E9nJm9BDpYM0=T5Me#YGwQv?pW",
+	}
 )
 
-func TestHashiCorpVaultKVV2Secret(t *testing.T) {
-	c,err := createVaultClient(socketPath)
-	if err != nil {
-		t.Error(err)
-		return
+// createVaultServer Starts local Vault server for testing purposes
+func createVaultServerAndClient(t *testing.T) (HashicorpVaultClient, *docker.DockerCluster) {
+	t.Helper()
+
+	opts := &docker.DockerClusterOptions{
+		ImageRepo: dockerImageRepo,
+		ImageTag:  dockerImageTag,
 	}
-	_, err = c.getVaultSecret("kvv2", testMount, testPath, []string{"password","username"})
+	cluster := docker.NewTestDockerCluster(t, opts)
+	client := cluster.Nodes()[0].APIClient()
+	hvc := HashicorpVaultClient{
+		client: client,
+		logger: slog.Default(),
+	}
+
+	return hvc, cluster
+}
+
+func writeTestKVSecret(t *testing.T, c HashicorpVaultClient) {
+	t.Helper()
+	_,err := c.client.KVv2(kvTestMount).Put(context.TODO(), kvTestPath, kvTestSecret)
+    if err != nil {
+        t.Fatal(err)
+    }
+}
+
+func TestHashiCorpVaultKVV2Secret(t *testing.T) {
+	c,cluster := createVaultServerAndClient(t)
+	defer cluster.Cleanup()
+	writeTestKVSecret(t, c)
+	_, err := c.getVaultSecret("kvv2", testMount, testPath, []string{"password","username"})
 	if err != nil {
 		t.Error(err)
 	}
@@ -50,4 +85,3 @@ func TestHashiCorpVaultUnsupportedSecret(t *testing.T) {
 		t.Error("Wrong error code, expected UnsupportedMountType")
 	}
 }
-
