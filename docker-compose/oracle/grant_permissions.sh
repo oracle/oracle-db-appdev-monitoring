053b8a8a0fd9e3a782b5c12553d11412fdcd6914
Anders Swanson <anders.swanson@oracle.com>
2026-04-23 10:59:00 -0700

    clean up compose scripts
    Signed-off-by: Anders Swanson <anders.swanson@oracle.com>

--- docker-compose/oracle/grant_permissions.sh
+++ docker-compose/oracle/grant_permissions.sh
@@ -1,5 +1,9 @@
+#!/bin/sh
+set -eu
+
+sqlplus -s / as sysdba <<EOF
 alter session set container=freepdb1;
-alter user pdbadmin identified by "Welcome12345";
+alter user pdbadmin identified by "${ORACLE_PASSWORD}";
 grant unlimited tablespace to pdbadmin;
 grant select_catalog_role to pdbadmin;
 grant execute on dbms_aq to pdbadmin;
@@ -8,6 +12,8 @@ grant execute on dbms_aqin to pdbadmin;
 grant execute on dbms_aqjms_internal to pdbadmin;
 grant execute on dbms_teqk to pdbadmin;
 grant execute on DBMS_RESOURCE_MANAGER to pdbadmin;
-grant select on sys.aq$_queue_shards to pdbadmin;
-grant select on sys.v_$diag_alert_ext to pdbadmin;
+grant select on sys.aq\$_queue_shards to pdbadmin;
+grant select on sys.v_\$diag_alert_ext to pdbadmin;
 grant select on user_queue_partition_assignment_table to pdbadmin;
+exit;
+EOF
